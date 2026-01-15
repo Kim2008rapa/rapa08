6f24badce873f61b259e8895b5a9ab847dc01873
Kim2008rapa <domera212008pa@gmail.com>
2026-01-14 21:44:37 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function mensaje() {
+  alert("Bienvenido a nuestra página de Identidad Institucional 💖");
+}
+function mensajeIdentidad() {
+  alert("La identidad institucional forma estudiantes con valores 💖");
+}
+function mensajeJuegos() {
+  alert("Aprender jugando fortalece la identidad institucional 🎮💗");
+}
+function mensajePublicidad() {
+  alert("La publicidad web comunica la identidad institucional 🌐💗");
+}
